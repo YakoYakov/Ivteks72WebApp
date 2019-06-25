303b30b78b09d6ef38459cd9e21562737ae72d8e
YakoYakov <yako.p.yakov@gmail.com>
2019-06-25 18:53:50 +0300

    Added Ivteks72DbContext to services. Added new faicon

--- Ivteks72.App/Startup.cs
+++ Ivteks72.App/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ivteks72.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -31,7 +32,7 @@ namespace Ivteks72.App
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.AddDbContext<Ivteks72DbContext>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
