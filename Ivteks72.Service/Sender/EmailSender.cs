f492c325ae2c30e1531195a47d46c9020aecc5e9
YakoYakov <yako.p.yakov@gmail.com>
2019-06-29 17:22:00 +0300

    Added implementation of EmailSender class

--- Ivteks72.Service/Sender/EmailSender.cs
+++ Ivteks72.Service/Sender/EmailSender.cs
@@ -1,13 +1,36 @@
 ﻿namespace Ivteks72.Service.Sender
 {
     using System;
+    using System.Net;
+    using System.Net.Mail;
     using System.Threading.Tasks;
 
     public class EmailSender : ISender
     {
+        private readonly string host;
+        private readonly int port;
+        private readonly bool enableSSL;
+        private readonly string username;
+        private readonly string password;
+
+        public EmailSender(string host, int port, bool enableSSL, string username, string password)
+        {
+            this.host = host;
+            this.port = port;
+            this.enableSSL = enableSSL;
+            this.username = username;
+            this.password = password;
+        }
+
         public Task SendMessageAsync(string email, string subject, string message)
         {
-            throw new NotImplementedException();
+            var client = new SmtpClient(this.host, this.port)
+            {
+                Credentials = new NetworkCredential(this.username, this.password),
+                EnableSsl = this.enableSSL
+            };
+
+            return client.SendMailAsync(new MailMessage(this.username, email, subject, message) { IsBodyHtml = true });
         }
     }
 }
