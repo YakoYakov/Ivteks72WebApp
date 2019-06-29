6d4929d400fcb89bf2de2498f9d9419dcbc8b8ac
YakoYakov <yako.p.yakov@gmail.com>
2019-06-29 17:10:48 +0300

    Added ISender Interface and Email Sender into Service

---
+++ Ivteks72.Service/Sender/EmailSender.cs
@@ -0,0 +1,13 @@
+﻿namespace Ivteks72.Service.Sender
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class EmailSender : ISender
+    {
+        public Task SendMessageAsync(string email, string subject, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ivteks72.Service/Sender/ISender.cs
@@ -0,0 +1,9 @@
+﻿namespace Ivteks72.Service.Sender
+{
+    using System.Threading.Tasks;
+
+    public interface ISender
+    {
+        Task SendMessageAsync(string email, string subject, string message);
+    }
+}
