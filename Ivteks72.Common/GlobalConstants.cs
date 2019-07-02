5646caae96e13d46f2f49d6c3c90f80efd4cf86a
YakoYakov <yako.p.yakov@gmail.com>
2019-07-02 19:17:34 +0300

    Aded globalConstats library. Added ApplicationRole seeder

---
+++ Ivteks72.Common/GlobalConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Ivteks72.Common
+{
+    public class GlobalConstants
+    {
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
---
+++ Ivteks72.Data/Seeding/RoleSeeder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ivteks72.Data.Seeding
+{
+    class RoleSeeder
+    {
+    }
+}
