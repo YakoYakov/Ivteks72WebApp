cdcea3d8186907338e045514e3e18d71116967c2
YakoYakov <yako.p.yakov@gmail.com>
2019-07-02 19:09:11 +0300

    Added ISeeder interface in data

---
+++ Ivteks72.Data/Seeding/ISeeder.cs
@@ -0,0 +1,10 @@
+﻿namespace Ivteks72.Data.Seeding
+{
+    using System;
+    using System.Threading.Tasks;
+ 
+    public interface ISeeder
+    {
+        Task SeedAsync(Ivteks72DbContext dbContext, IServiceProvider serviceProvider);
+    }
+}
