97d9a0bcb1bab9bc100df9d66e263b778e9c5863
YakoYakov <yako.p.yakov@gmail.com>
2019-07-07 13:08:18 +0300

    Added OrderController

---
+++ Ivteks72.App/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿namespace Ivteks72.App.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class OrderController : Controller
+    {
+        public IActionResult ViewAll()
+        {
+            return this.View();
+        }
+    }
+}
