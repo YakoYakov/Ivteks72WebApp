0f0ff568fce68fb86909d7d03687825c30eecb04
YakoYakov <yako.p.yakov@gmail.com>
2019-07-07 12:46:04 +0300

    Added GetUserById method in UserService

--- Ivteks72.Service/UserService.cs
+++ Ivteks72.Service/UserService.cs
@@ -1,12 +1,23 @@
 ﻿namespace Ivteks72.Service
 {
+    using System.Linq;
+    using Ivteks72.Data;
     using Ivteks72.Domain;
 
     public class UserService : IUserService
     {
+        private readonly Ivteks72DbContext context;
+
+        public UserService(Ivteks72DbContext context)
+        {
+            this.context = context;
+        }
+
         public ApplicationUser GetUserById(string id)
         {
-            throw new System.NotImplementedException();
+            var userFromDb = this.context.ApplicationUsers.FirstOrDefault(user => user.Id == id);
+
+            return userFromDb;
         }
     }
 }
