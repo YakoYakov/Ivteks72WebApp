57054401bc6e72e7bfa62678270c401059687c3c
YakoYakov <yako.p.yakov@gmail.com>
2019-07-07 18:34:37 +0300

    Added Accepted, Finished,Pending, Rejected view models

--- Ivteks72.App/Models/Order/OrderByStatusViewModel.cs
+++ Ivteks72.App/Models/Order/OrderByStatusViewModel.cs
@@ -1,24 +1,21 @@
 ﻿namespace Ivteks72.App.Models.Order
 {
     using System;
-    using System.ComponentModel.DataAnnotations;
-    using Ivteks72.Domain;
-    using Ivteks72.Domain.Enums;
   
     public class OrderByStatusViewModel
     {
-        [Required]
+        public string Clothing { get; set; }
+
+        public int Quantity { get; set; }
+
         public DateTime IssuedOn { get; set; }
 
-        [Required]
-        public OrderStatus Status { get; set; }
-
-        [Required]
-        public string ClothingId { get; set; }
-        public Clothing Clothing { get; set; }
-
-        [Required]
         public string IssuerId { get; set; }
-        public ApplicationUser Issuer { get; set; }
+
+        public string Company { get; set; }
+
+        public decimal TotalOrderPrice { get; set; }
+
+        public string Status { get; set; }
     }
 }
