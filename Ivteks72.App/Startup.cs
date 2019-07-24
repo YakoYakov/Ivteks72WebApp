3c934697e96495fea5140232c999927d70f4d597
YakoYakov <yako.p.yakov@gmail.com>
2019-07-24 18:37:40 +0300

    Added confirm email view in identity added logic for the confirmation email

--- Ivteks72.App/Startup.cs
+++ Ivteks72.App/Startup.cs
@@ -31,12 +31,10 @@
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
@@ -46,7 +44,10 @@
                     Configuration["ConnectionStrings:DefaultConnection"])
                 );
 
-            services.AddIdentity<ApplicationUser, ApplicationRole>()
+            services.AddIdentity<ApplicationUser, ApplicationRole>(config => 
+            {
+                config.SignIn.RequireConfirmedEmail = true;
+            })
                 .AddEntityFrameworkStores<Ivteks72DbContext>()
                 .AddDefaultTokenProviders();
 
@@ -83,12 +84,9 @@
 
             services.Configure<MessageSenderOptions>(Configuration);
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);          
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
@@ -114,7 +112,6 @@
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
