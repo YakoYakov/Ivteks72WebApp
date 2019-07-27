41eca5ff80c6db3e16459f6559d8e8cf7d88a275
YakoYakov <yako.p.yakov@gmail.com>
2019-07-27 14:00:36 +0300

    Added printing functionality in invoice details view

--- Ivteks72.App/wwwroot/js/site.js
+++ Ivteks72.App/wwwroot/js/site.js
@@ -2,7 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$('#printInvoice').click(function () {
+
+$('#invoice').click(function () {
     Popup($('.invoice')[0].outerHTML);
     function Popup(data) {
         window.print();
