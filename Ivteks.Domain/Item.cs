e5c8828a475d1ccf35e126237c5a8319dafde4bd
YakoYakov <yako.p.yakov@gmail.com>
2021-03-23 14:00:31 +0200

    Added migration and updated database to include Items table

--- Ivteks.Domain/Item.cs
+++ Ivteks.Domain/Item.cs
@@ -7,6 +7,8 @@ namespace Ivteks72.Domain
         public string Id { get; set; }
         public string Name { get; set; }
 
+        public int Quantity { get; set; }
+
         [Required]
         [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
         public decimal Price { get; set; }
