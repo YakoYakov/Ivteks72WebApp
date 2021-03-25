fd22f45ce2d8fed8e515534de1237855f9640f50
YakoYakov <yako.p.yakov@gmail.com>
2021-03-25 19:02:43 +0200

    Added preliminary work to items code logic

--- Ivteks72.App/Controllers/ItemController.cs
+++ Ivteks72.App/Controllers/ItemController.cs
@@ -1,15 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Ivteks72.App.Models.Item;
+using Ivteks72.Data;
+using Ivteks72.Domain;
+using Ivteks72.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace Ivteks72.App.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly Ivteks72DbContext context;
+        private readonly ICloudinaryService cService;
+        public ItemController(Ivteks72DbContext context, ICloudinaryService cService)
+        {
+            this.context = context;
+            this.cService = cService;
+        }
+        // TODO maybe remove?
         public IActionResult ItemsList()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddNewItem()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNewItem(AddItemViewModel model, IFormFile photo)
+        {
+            if (photo == null || photo.Length == 0)
+            {
+                this.ModelState.AddModelError("picError", "You have to provide a photo");
+                return this.View(model);
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var itemUrl = await this.cService.UploadDiagramImage(photo, photo.FileName);
+
+            // TODO make it through service!!!
+            Item item = new Item()
+            {
+                Type = model.Type,
+                Name = model.Name,
+                Description = model.Description,
+                Price = model.Price,
+                Quantity = model.Quantity,
+                ImageUrl = itemUrl
+            };
+
+            await this.context.Items.AddAsync(item);
+            await this.context.SaveChangesAsync();
             return View();
         }
     }
--- Ivteks72.App/Models/Item/AddItemViewModel.cs
+++ Ivteks72.App/Models/Item/AddItemViewModel.cs
@@ -1,23 +1,27 @@
 ﻿using Ivteks72.Domain.Enums;
+using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Ivteks72.Domain
+namespace Ivteks72.App.Models.Item
 {
-    public class Item
+    public class AddItemViewModel
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public string Id { get; set; }
-
+        [Required]
         public ItemType Type { get; set; }
-        public string Name { get; set; }
 
-        public int Quantity { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 3)]
+        public string Name { get; set; }
 
         [Required]
         [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
         public decimal Price { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+
         public string Description { get; set; }
+
         public string ImageUrl { get; set; }
     }
 }
